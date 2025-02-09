ed6c0af883e61d9f2fbf3ae0dbd574c386d8fdf1
Kilian Schulte <git@schultek.dev>
2025-02-09 11:21:57 +0100

    fix template

--- templates/jaspr/idx-template.nix
+++ templates/jaspr/idx-template.nix
@@ -6,6 +6,7 @@
   bootstrap = ''    
     dart pub global activate jaspr_cli
     echo "export PATH=\"$PATH\":\"$HOME/.pub-cache/bin\" ">> ~/.bashrc
+    source ~/.bashrc
     jaspr create --mode=${mode} --routing=${routing} \
       --flutter=${if flutter == "true" then "embedded" else if plugins == "true" then "plugins-only" else "none"} \
       --backend=none "$WS_NAME"
